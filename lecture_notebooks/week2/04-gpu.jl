ec8a245e32e9f9dc709fdac2f2906c1d4789795e
phunc20 <wucf20@gmail.com>
2021-01-25 12:34:28 +0700

    seems to work; need some cleaning of the code later

--- lecture_notebooks/week2/04-gpu.jl
+++ lecture_notebooks/week2/04-gpu.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.11.14
+# v0.12.18
 
 using Markdown
 using InteractiveUtils
@@ -13,13 +13,21 @@ begin
   
   #Pkg.add("Colors")
   Pkg.add("Images")
+  Pkg.add("ImageIO")
   #Pkg.add("ImageMagick")
   #Pkg.add("ImageFiltering")
+  Pkg.add("StaticArrays")
+  Pkg.add("CUDA")
+  Pkg.add("OffsetArrays")
   
   using Images
   #using PlutoUI
   #using ImageMagick
   #using ImageFiltering
+  using SparseArrays
+  using CUDA
+  using StaticArrays
+  using OffsetArrays
 end
 
 # ╔═╡ 52d6a726-5e27-11eb-03a9-410141811719
@@ -39,18 +47,6 @@ Julia can run on the CPU or GPU. Such portability is the holy grail of HPC compu
 
 The user decides where the data lives and the computation follows!
 "
-
-# ╔═╡ f730aa02-5e26-11eb-1d0a-19ed64679ea2
-#convolve2D.(    range,   ((padded_img),), (filter,));
-#convolve2D.(cu(range), (cu(padded_img),), (filter,));
-
-# ╔═╡ f6aa7824-5e26-11eb-0da9-b97c615448d1
-# CPU
-#@elapsed(bench(blurred, padded_img, range, filter)
-
-# ╔═╡ fff0ca24-5e27-11eb-2a2c-f75b443f7197
-# GPU
-#@elapsed(CUDA.@sync bench(gpu_blurred, gpu_img, gpu_range, filter)
 
 # ╔═╡ ffcfc534-5e27-11eb-0454-a7d6879ca011
 
@@ -79,15 +75,22 @@ the convolution operation.
 
 # ╔═╡ aae9636c-5e28-11eb-00fb-17900f53bf4e
 begin
-  G(x, y, σ=0.5) = 1/sqrt(2*π*σ^2) * e^(-(x^2+y^2)/(2*σ^2))
-  G(I::CartesianIndices{2}, σ=0.5) = G(I.I..., σ)
+  G(x, y, σ=0.5) = 1/sqrt(2*π*σ^2) * ℯ^(-(x^2+y^2)/(2*σ^2))
+  G(I::CartesianIndex{2}, σ=0.5) = G(I.I..., σ)
 end
+
+# ╔═╡ 9fe43544-5e37-11eb-2d54-814a6dc8dcf9
+4*ceil(Int,σ)+1
 
 # ╔═╡ 0adb8318-5e29-11eb-25a2-c5a439acb115
 function gaussian_filter(σ, l=4*ceil(Int,σ)+1)
   w = l ÷ 2
   # SMatrix: A performance hack here.
-  gauss = SMatrix{1,1}(map(i->G(i, σ), CartesianIndices((-w:w, -w:w))))
+  gauss = SMatrix{l,l}(map(i->G(i, σ), CartesianIndices((-w:w, -w:w))))
+  #gauss = SparseMatrix{1,1}(map(i->G(i, σ), CartesianIndices((-w:w, -w:w))))
+  #gauss = sparse(Matrix{1,1}(map(i->G(i, σ), CartesianIndices((-w:w, -w:w)))))
+  #gauss = AbstractSparseMatrix{1,1}(map(i->G(i, σ), CartesianIndices((-w:w, -w:w))))
+  #gauss = SparseMatrixCSC{1,1}(map(i->G(i, σ), CartesianIndices((-w:w, -w:w))))
   gauss ./ sum(gauss)
 end
 
@@ -107,13 +110,18 @@ function convolve2D(I, A, filter)  # one convolution at I
   sum(data .* filter)  # here's the math
 end
 
-# ╔═╡ 0a890912-5e29-11eb-0a7a-4ba3e9ff6da1
+# ╔═╡ 13fb75d8-5e2f-11eb-1365-b3f0988ebd36
+md"
+Note that we don't want to copy (That's why we use `view`) because copying data is very, very expensive.
+"
 
+# ╔═╡ d0169b0a-5e32-11eb-12b6-e1f04007c3e8
+filter = gaussian_filter(σ)
+
+# ╔═╡ 0a890912-5e29-11eb-0a7a-4ba3e9ff6da1
+md"## Padding the data"
 
 # ╔═╡ 0a69da56-5e29-11eb-1983-15df52de477e
-
-
-# ╔═╡ 56088d28-5bb5-11eb-00ce-2170b0c6ecc0
 function pad(data, border=1)
   padded_size = map(s->s+2*border, size(data))
   padded_range = map(s->(1-border):(s+border), size(data))
@@ -124,11 +132,85 @@ function pad(data, border=1)
   odata, collect(range)
 end
 
-# ╔═╡ 5eb5e576-5bb5-11eb-265c-c339f547c12b
+# ╔═╡ 1a9706d6-5e30-11eb-2da1-f9ef48542a18
 padded_img, range = pad(img, size(filter, 1) ÷ 2);
 
-# ╔═╡ 5ef67d3e-5bb5-11eb-29ff-556cdba4b5b0
+# ╔═╡ f730aa02-5e26-11eb-1d0a-19ed64679ea2
+#convolve2D.(    range,   ((padded_img),), (filter,));
+convolve2D.(cu(range), (cu(padded_img),), (filter,));
+
+# ╔═╡ 1ac7ef1c-5e30-11eb-315f-b3b95d978c45
 gpu_range = cu(range);
+
+# ╔═╡ 1a6a7a62-5e30-11eb-0ad9-95f4618449c3
+
+
+# ╔═╡ bea222de-5e34-11eb-0af7-f50199286b50
+
+
+# ╔═╡ be6af5a4-5e34-11eb-1d9d-ed275b86eb73
+begin
+  blurred = similar(img)
+  blurred .= convolve2D.(range, (padded_img,), (filter,))
+end
+
+# ╔═╡ be34d2f6-5e34-11eb-11d7-91dacc81ae0c
+begin
+  # Really need to finish https://github.com/JuliaArrays/OffsetArrays.jl/pull/57
+  import CUDA.Adapt
+  Adapt.adapt_structure(to, x::OffsetArray) = OffsetArray(Adapt.adapt(to, parent(x)), x.offsets)
+  Base.Broadcast.BroadcastStyle(::Type{<:OffsetArray{<:Any, <:Any, AA}}) where AA = Base.Broadcast.BroadcastStyle(AA)
+  forcerun = nothing
+end
+
+# ╔═╡ be002efc-5e34-11eb-28fc-9f90f4adee4b
+md"
+## Executing the code on the GPU
+First we need to move the data using the `cu` function to the GPU.
+"
+
+# ╔═╡ bdc5c71c-5e34-11eb-0ae8-cfd65b7739c2
+begin
+  forcerun  # ignore
+  gpu_img = cu(padded_img)
+end
+
+# ╔═╡ 5266e61c-5e35-11eb-2a6d-6ff64279f604
+typeof(gpu_img)
+
+# ╔═╡ 5236a330-5e35-11eb-0706-412d909cd8be
+gpu_blurred = cu(similar(img, RGBA{Float64}));  # allocate output
+
+# ╔═╡ 5201822c-5e35-11eb-2f0c-996d4886e708
+gpu_blurred .= convolve2D.(gpu_range, (gpu_img,), (filter,));
+
+# ╔═╡ 9434bdee-5e35-11eb-38a7-077e382f5360
+Array(gpu_blurred)
+
+# ╔═╡ 9402dee6-5e35-11eb-0f75-1d91e0f91e3a
+function bench(out, img, range, filter)
+  out .= convolve2D.(range, (img,), (filter,))
+end
+
+# ╔═╡ f6aa7824-5e26-11eb-0da9-b97c615448d1
+# CPU
+@elapsed(bench(blurred, padded_img, range, filter))
+
+# ╔═╡ fff0ca24-5e27-11eb-2a2c-f75b443f7197
+# GPU
+@elapsed(CUDA.@sync bench(gpu_blurred, gpu_img, gpu_range, filter))
+
+# ╔═╡ bd9484bc-5e35-11eb-0f71-2b1c254d94a0
+md"Setting up the environment:"
+
+# ╔═╡ 79966884-5e3a-11eb-2ba4-25c923de49bf
+
+
+# ╔═╡ 5ef67d3e-5bb5-11eb-29ff-556cdba4b5b0
+
+
+# ╔═╡ 5eb5e576-5bb5-11eb-265c-c339f547c12b
+
 
 # ╔═╡ Cell order:
 # ╟─52d6a726-5e27-11eb-03a9-410141811719
@@ -143,15 +225,32 @@ gpu_range = cu(range);
 # ╠═38869270-5baa-11eb-25eb-83cddddac6dc
 # ╠═84fd13a6-5baa-11eb-3dca-fddfcc6f03d9
 # ╠═32e9445e-5bad-11eb-0d67-d1798bb221ce
-# ╟─5c5d695a-5e28-11eb-24a8-6bb3c012ec2d
+# ╠═5c5d695a-5e28-11eb-24a8-6bb3c012ec2d
 # ╠═aae9636c-5e28-11eb-00fb-17900f53bf4e
+# ╠═9fe43544-5e37-11eb-2d54-814a6dc8dcf9
 # ╠═0adb8318-5e29-11eb-25a2-c5a439acb115
 # ╠═0abbec60-5e29-11eb-3872-112430e3f0d3
 # ╠═0aa584fc-5e29-11eb-1036-73bfd7ab888d
+# ╟─13fb75d8-5e2f-11eb-1365-b3f0988ebd36
+# ╠═d0169b0a-5e32-11eb-12b6-e1f04007c3e8
 # ╠═0a890912-5e29-11eb-0a7a-4ba3e9ff6da1
 # ╠═0a69da56-5e29-11eb-1983-15df52de477e
+# ╠═1ac7ef1c-5e30-11eb-315f-b3b95d978c45
+# ╠═1a9706d6-5e30-11eb-2da1-f9ef48542a18
+# ╠═1a6a7a62-5e30-11eb-0ad9-95f4618449c3
+# ╠═bea222de-5e34-11eb-0af7-f50199286b50
+# ╠═be6af5a4-5e34-11eb-1d9d-ed275b86eb73
+# ╠═be34d2f6-5e34-11eb-11d7-91dacc81ae0c
+# ╟─be002efc-5e34-11eb-28fc-9f90f4adee4b
+# ╠═bdc5c71c-5e34-11eb-0ae8-cfd65b7739c2
+# ╠═5266e61c-5e35-11eb-2a6d-6ff64279f604
+# ╠═5236a330-5e35-11eb-0706-412d909cd8be
+# ╠═5201822c-5e35-11eb-2f0c-996d4886e708
+# ╠═9434bdee-5e35-11eb-38a7-077e382f5360
+# ╠═9402dee6-5e35-11eb-0f75-1d91e0f91e3a
+# ╠═bd9484bc-5e35-11eb-0f71-2b1c254d94a0
 # ╠═a9fbe424-5bae-11eb-39fc-614d047ec480
 # ╠═7caf8746-5bae-11eb-1ed8-eb6b32e6a7e6
-# ╠═56088d28-5bb5-11eb-00ce-2170b0c6ecc0
+# ╠═79966884-5e3a-11eb-2ba4-25c923de49bf
 # ╠═5ef67d3e-5bb5-11eb-29ff-556cdba4b5b0
 # ╠═5eb5e576-5bb5-11eb-265c-c339f547c12b
--- lecture_notebooks/week2/trash.jl
+++ lecture_notebooks/week2/trash.jl
@@ -34,32 +34,11 @@ md"
 
 
 
+Pkg.add("AbstractArray")
 
 
-
-function pad(data, border=1)
-  padded_size = map(s->s+2*border, size(data))
-  padded_range = map(s->(1-border):(s+border), size(data))
-  range = CartesianIndices(map(s->1:s, size(data)))
-
-  odata = OffsetArray(zeros(eltype(data), padded_size), padded_range...)
-  odata[range] = data
-  odata, collect(range)
-end
 
 gpu_range = cu(range);
 
 padded_img, range = pad(img, size(filter, 1) ÷ 2);
-
-begin
-  blurred = similar(img)
-  blurred .= convolve2D.(range, (padded_img,), (filter,))
-end
-
-begin
-  # Really need to finish https://github.com/JuliaArrays/OffsetArrays.jl/pull/57
-  import CUDA.Adapt
-  Adapt.adapt_structure(to, x::OffsetArray) = OffsetArray(Adapt.adapt(to, parent(x)), x.offsets)
-  Base.Broadcast.BroadcastStyle(::Type{<:OffsetArray{<:Any, <:Any, AA}}) where AA = Base.Broadcast.BroadcastStyle(AA)
-  forcerun = nothing
-end
+SparseMatrixCSC
